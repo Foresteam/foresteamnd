16f6fcb5a163cfffd3bdc0a224119e4b7c0b0471
Foresteam <heler1141@gmail.com>
2021-11-16 20:23:06 +0300

    Stack docs

--- src/Stack.h
+++ src/Stack.h
@@ -15,21 +15,34 @@ private:
 	Node<Item>* self;
 	size_t size;
 public:
+	/// @brief Swaps two nodes
 	static void Swap(Node<Item>* a, Node<Item>* b);
 
 	Stack();
 
+	/// @brief Adds an item
 	void Push(Item item);
+	/// @brief Drops the top item
 	/// @throws StackUnderflow
 	void Drop();
+	/// @brief Drops the top item and returns it
+	/// @returns The top item
 	/// @throws StackUnderflow
 	Item Pop();
+	/// @brief Returns the top item without dropping it
+	/// @returns The top item
 	Item* Peek();
+	/// @brief Duplicates the top item
 	void Dup();
+	/// @brief Swaps the top two items
 	void Swap();
+	/// @brief Rotates the top three items: 123 => 231
 	void Rot();
+	/// @brief Flips the stack
 	void Flip();
+	/// @returns If the stack is empty
 	bool IsEmpty();
 
+	/// @returns The size of the stack (number of items)
 	size_t GetSize();
 };
