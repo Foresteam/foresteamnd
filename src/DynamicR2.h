b57ba808df529a6fe27cb1087d14b74ad41d5da3
Foresteam <heler1141@gmail.com>
2021-11-19 15:11:48 +0300

    DynamicR2 docs

--- src/DynamicR2.h
+++ src/DynamicR2.h
@@ -1,5 +1,6 @@
 #include <cstdlib>
 
+/// @brief Dynamic array [][]. Better create with 'new'/'malloc'
 template <typename T>
 class DynamicR2 {
 private:
@@ -7,6 +8,8 @@ private:
 	T** self;
 	size_t s1, s2;
 public:
+    /// @param s1 Size of low level
+    /// @param s2 Size of high level
 	DynamicR2(size_t s1, size_t s2);
 	DynamicR2(const DynamicR2& other);
 
